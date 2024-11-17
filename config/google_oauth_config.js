9f2d1d75998d8c83934ddcbb9b6dce4528ead7a1
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-11-17 14:08:58 +0530

    fixed oauth url

--- config/google_oauth_config.js
+++ config/google_oauth_config.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback"
+  callbackURL: `${process.env.FRONTEND_URL}/auth/google/callback`
 },
   async function (accessToken, refreshToken, profile, cb) {
     try {
